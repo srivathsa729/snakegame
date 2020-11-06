0a12232454d0dea4e02c184d979909fe89c7558e
srivathsa729 <srivathsa729.8@gmail.com>
2020-11-06 11:49:17 +0530

    implemented pygame window

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+dis = pygame.display.set_mode((400, 400))
+pygame.display.update()
+pygame.display.set_caption("Snake Game")
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+pygame.quit()
+quit()
